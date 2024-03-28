9680e0160f80996ca9fe25c184340eb66d94c144
dunking <gavinjdunkin@gmail.com>
2024-03-27 22:13:01 -0400

    moved locations

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
-import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
 const app = express();
 app.use(cors());
